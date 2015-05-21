a0e072d8564b2a74ca0609e80291aaca9ac1dac0
Jelsmaa <jellehoekstra1@gmail.com>
2015-05-21 17:21:28 +0200

    Feestje

---
+++ database.php
@@ -0,0 +1,16 @@
+<?php
+/* basis SQL settings en functies */
+$dbbhost = 'localhost';
+$dbbuser = 'yeti';
+$dbbpass = 'yetiismooi';
+$dbbname = 'yetiscratch'; 
+	
+function open_database(){
+	$dbconn = mysql_connect($dbbhost, $dbbuser, $dbbpass) or die("Couldn't connect to SQL Server"); 
+	mysql_select_db($dbbname);
+}
+
+function close_database(){
+	mysql_close($dbconn);
+}
+?>
