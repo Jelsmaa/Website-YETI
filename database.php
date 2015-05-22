f78eb1a8af39aaa3d6727bbd2a270c60b0d26a7d
Jelsmaa <jellehoekstra1@gmail.com>
2015-05-22 21:19:07 +0200

    Updates SQL en beveiliging

--- database.php
+++ database.php
@@ -12,4 +12,14 @@ function open_database(){
 function close_database(){
 	mysql_close($dbconn);
 }
+
+function generateRandomString($length = 10) {
+    $characters = '0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ';
+    $charactersLength = strlen($characters);
+    $randomString = '';
+    for ($i = 0; $i < $length; $i++) {
+        $randomString .= $characters[rand(0, $charactersLength - 1)];
+    }
+    return $randomString;
+}
 ?>
